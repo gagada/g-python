d73b1c7d327d4cc62d6ee5499a1e2d944044e0f4
Gerard C. Agada <gerard.agada@hotmail.com>
2017-09-02 02:15:12 -0400

    Update lessThanTen1.py

--- lessThanTen1.py
+++ lessThanTen1.py
@@ -3,7 +3,7 @@
 a = [1,1,2,3,5,8,13,21,34,55,89,100,150,14,65,7,67,18,200]
 x = []
 
-#Ask the user for a umber and returna list that contains only elements
+#Ask the user for a number and return a list that contains only elements
 #from the original list and that are smaller than that number given by 
 #the user
 userInput = raw_input('Please Enter a Number: ')
